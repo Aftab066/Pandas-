042599bc84854e861f0fddfed6905918b1ee8a94
Aftab Mulani <aftabmulani1739@gmail.com>
2026-02-07 22:15:55 +0530

    a

--- 01_Intro.py
+++ 01_Intro.py
@@ -1,1 +1,7 @@
-print("hey")
+import pandas as pd 
+
+# df = pd.read_csv("file.csv") #To Load CSV Files
+df = pd.read_excel("file.csv") #To Load excel Files
+df = pd.read_json("file.csv") #To Load json Files
+
+print(df)
